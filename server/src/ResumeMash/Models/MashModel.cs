17ad4de554fd1e06e7c8187dba540b8c78338dbb
Jordan Sim-Smith <jordansimsmith@gmail.com>
2021-02-28 18:38:16 +1300

    Implement getting a mash of two random resumes

---
+++ server/src/ResumeMash/Models/MashModel.cs
@@ -0,0 +1,8 @@
+namespace ResumeMash.Models
+{
+    public class MashModel
+    {
+        public ResumeModel FirstResume { get; set; }
+        public ResumeModel SecondResume { get; set; }
+    }
+}
