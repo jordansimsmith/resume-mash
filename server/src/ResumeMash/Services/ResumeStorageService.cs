536cdd7bd709422552cdd6dc4247f2e6f766c472
Jordan Sim-Smith <jordansimsmith@gmail.com>
2021-02-28 14:57:23 +1300

    Implement resume upload to S3

--- server/src/ResumeMash/Services/ResumeStorageService.cs
+++ server/src/ResumeMash/Services/ResumeStorageService.cs
@@ -1,14 +1,38 @@
 using System.IO;
 using System.Threading.Tasks;
+using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.Extensions.Configuration;
 
 namespace ResumeMash.Services
 {
-    public class ResumeStorageService: IResumeStorageService
+    public class ResumeStorageService : IResumeStorageService
     {
-        public Task UploadResumeAsync(Stream resumeStream, string resumeKey)
+        private readonly IAmazonS3 _s3Client;
+        private readonly string _bucketName;
+
+        public ResumeStorageService(IConfiguration configuration)
         {
-            // TODO: implement S3 upload
-            return Task.CompletedTask;
+            var awsAccessKeyId = configuration["S3:AWS_ACCESS_KEY_ID"];
+            var awsSecretKey = configuration["S3:AWS_SECRET_KEY"];
+            var awsS3Region = configuration["S3:Region"];
+            var awsCredentials = new BasicAWSCredentials(awsAccessKeyId, awsSecretKey);
+            _s3Client = new AmazonS3Client(awsCredentials, RegionEndpoint.GetBySystemName(awsS3Region));
+            _bucketName = configuration["S3:BucketName"];
+        }
+
+        public async Task UploadResumeAsync(Stream resumeStream, string resumeKey)
+        {
+            var putObjectRequest = new PutObjectRequest
+            {
+                BucketName = _bucketName,
+                Key = resumeKey,
+                InputStream = resumeStream,
+                ContentType = "application/pdf"
+            };
+            await _s3Client.PutObjectAsync(putObjectRequest);
         }
     }
 }
