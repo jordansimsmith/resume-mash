e105b6ee0ddabb80dc8b86cb48d38d645b14f9ba
Jordan Sim-Smith <jordansimsmith@gmail.com>
2021-06-13 21:08:07 +1200

    Design HeaderBar component using chakra ui

--- client/components/HeaderBar.tsx
+++ client/components/HeaderBar.tsx
@@ -1,6 +1,16 @@
-import { useUser } from '@auth0/nextjs-auth0';
-import { useRouter } from 'next/router';
 import React from 'react';
+import NextLink from 'next/link';
+import { useRouter } from 'next/router';
+import { useUser } from '@auth0/nextjs-auth0';
+import { Button } from '@chakra-ui/button';
+import {
+  Box,
+  Container,
+  Heading,
+  Wrap,
+  WrapItem,
+  Link,
+} from '@chakra-ui/layout';
 
 export const HeaderBar: React.FC<{}> = () => {
   const { user } = useUser();
@@ -8,14 +18,44 @@ export const HeaderBar: React.FC<{}> = () => {
 
   const handleLogin = () => router.push('/api/auth/login');
   const handleLogout = () => router.push('/api/auth/logout');
+  const handleUpload = () => router.push('/resumes/new');
 
   return (
-    <nav>
-      {user ? (
-        <button onClick={handleLogout}>Logout</button>
-      ) : (
-        <button onClick={handleLogin}>Log in</button>
-      )}
-    </nav>
+    <Box backgroundColor="purple.100" padding="10px">
+      <Container maxW="container.xl">
+        <Wrap justify="space-between" spacing="0">
+          <WrapItem>
+            <Link as={NextLink} href="/">
+              <Heading cursor="pointer">Resume Mash</Heading>
+            </Link>
+          </WrapItem>
+
+          <WrapItem>
+            <Wrap>
+              {user && (
+                <WrapItem>
+                  <Button
+                    variant="ghost"
+                    colorScheme="purple"
+                    onClick={handleUpload}
+                  >
+                    Upload
+                  </Button>
+                </WrapItem>
+              )}
+              <WrapItem>
+                <Button
+                  variant="ghost"
+                  colorScheme="purple"
+                  onClick={user ? handleLogout : handleLogin}
+                >
+                  {user ? 'Logout' : 'Log in'}
+                </Button>
+              </WrapItem>
+            </Wrap>
+          </WrapItem>
+        </Wrap>
+      </Container>
+    </Box>
   );
 };
