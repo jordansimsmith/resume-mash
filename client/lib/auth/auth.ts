e60f4db4fd7bd2013c81f74e200df309681e4ff4
Jordan Sim-Smith <jordansimsmith@gmail.com>
2021-06-16 16:51:38 +1200

    Fix bug where unauthenticated users could not send graphql queries

--- client/lib/auth/auth.ts
+++ client/lib/auth/auth.ts
@@ -1,7 +1,11 @@
 export async function getAccessToken(): Promise<string> {
-  const url = 'http://localhost:3000/api/auth/token';
-  const res = await fetch(url);
-  const { accessToken } = await res.json();
+  try {
+    const url = 'http://localhost:3000/api/auth/token';
+    const res = await fetch(url);
+    const { accessToken } = await res.json();
 
-  return accessToken;
+    return accessToken;
+  } catch {
+    return null;
+  }
 }
